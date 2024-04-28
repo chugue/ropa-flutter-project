6c8bda49f7bca478be5502d2982c42f65758dcd8
LifeIsOne <didtmdgh258@gmail.com>
2024-04-28 19:01:39 +0900

    코디 페이지 제작

--- lib/models/codi.dart
+++ lib/models/codi.dart
@@ -1,10 +1,12 @@
 class Codi {
   int id;
   List<String> mainImg;
+  List<String> itemImg;
 
   Codi({
     required this.id,
     required this.mainImg,
+    required this.itemImg,
   });
 }
 
@@ -13,10 +15,17 @@ List<Codi> codiList = [
     "assets/images/codi/codi01_01.jpg",
     "assets/images/codi/codi01_02.jpg",
     "assets/images/codi/codi01_03.jpg",
+  ], itemImg: [
+    "assets/images/item/item_01.png",
+    "assets/images/item/item_02.jpg",
+    "assets/images/item/item_03.png",
   ]),
   Codi(id: 2, mainImg: [
     "assets/images/codi/codi02_01.jpg",
     "assets/images/codi/codi02_02.jpg",
     "assets/images/codi/codi02_03.jpg",
+  ], itemImg: [
+    "assets/images/item/item_01.jpg",
+    "assets/images/item/item_02.jpg",
   ]),
 ];
