51a5096b8a8a0f749a161e4ae6b2791ee78d36c2
LifeIsOne <didtmdgh258@gmail.com>
2024-05-06 00:53:33 +0900

    문의하기 컴포넌트 분리

--- lib/ui/holder/inquiry/inquiry_insert_page/components/inquiry_insert_body.dart
+++ lib/ui/holder/inquiry/inquiry_insert_page/components/inquiry_insert_body.dart
@@ -1,5 +1,5 @@
-import 'package:final_project_team02/models/brand.dart';
 import 'package:final_project_team02/ui/components/screen_size.dart';
+import 'package:final_project_team02/ui/holder/inquiry/inquiry_insert_page/components/InquiryBrandSelect.dart';
 import 'package:final_project_team02/ui/holder/inquiry/inquiry_insert_page/components/inquiry_insert_button.dart';
 import 'package:final_project_team02/ui/holder/inquiry/inquiry_insert_page/components/inquiry_inset_content.dart';
 import 'package:final_project_team02/ui/holder/inquiry/inquiry_insert_page/components/inquiry_inset_title.dart';
@@ -17,27 +17,7 @@ class InquiryInsertBody extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text(
-            "문의할 브랜드 선택*",
-            style: TextStyle(color: Colors.black),
-          ),
-          DropdownMenu<String>(
-            width: screenWidth * 0.93,
-            initialSelection: "브랜드를 선택해주세요!",
-            onSelected: (String? value) {},
-            dropdownMenuEntries: [
-              DropdownMenuEntry<String>(
-                value: "브랜드를 선택해주세요!",
-                label: "브랜드를 선택해주세요!",
-              ),
-              ...brands.map<DropdownMenuEntry<String>>((Brand brand) {
-                return DropdownMenuEntry<String>(
-                  value: brand.name,
-                  label: brand.name,
-                );
-              }).toList(),
-            ],
-          ),
+          InquiryBrandSelect(screenWidth: screenWidth),
           SizedBox(height: 16),
           InquiryInsertTitle(),
           SizedBox(height: 16),
