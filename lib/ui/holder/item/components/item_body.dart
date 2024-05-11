dad6ca0e3b964a0fa99dccf6952f830e14aafe1b
kjh5848 <kjh5848@gmail.com>
2024-05-12 01:17:46 +0900

    아이템 리스트 뿌리기 CustomScrollView 스크롤 수정

--- lib/ui/holder/item/components/item_body.dart
+++ lib/ui/holder/item/components/item_body.dart
@@ -23,83 +23,80 @@ class ItemBody extends ConsumerWidget {
       slivers: [
         // 이미지 슬라이더를 위한 SliverList
         SliverToBoxAdapter(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.width, // 미디어 쿼리 대신 고정 높이 사용
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: model.mainPhotos.length,
-              itemBuilder: (context, index) {
-                return Image.memory(
-                  Base64Decoder()
-                      .convert(model.mainPhotos[index].mainPhotoBase64),
-                  fit: BoxFit.cover,
-                );
-              },
-            ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.width, // 미디어 쿼리 대신 고정 높이 사용
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: model.mainPhotos.length,
+                  itemBuilder: (context, index) {
+                    return Image.memory(
+                      Base64Decoder()
+                          .convert(model.mainPhotos[index].mainPhotoBase64),
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "[${model.item.brandName}] ",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '${model.item.brandName}',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${model.item.discountPrice}원",
+                      style: TextStyle(fontSize: 18, color: Colors.red),
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      "${model.item.price}원",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.lineThrough),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 60),
+              SizedBox(
+                height:
+                    MediaQuery.of(context).size.height, // 미디어 쿼리 대신 고정 높이 사용
+                child: ListView.builder(
+                  itemCount: model.detailPhotos.length,
+                  itemBuilder: (context, index) {
+                    return Image.memory(
+                      Base64Decoder()
+                          .convert(model.detailPhotos[index].subPhotoBase64),
+                      fit: BoxFit.fill,
+                    );
+                  },
+                ),
+              )
+            ],
           ),
         ),
         // 제품 브랜드 및 이름을 위한 SliverToBoxAdapter
-        SliverToBoxAdapter(
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "[${model.item.brandName}] ",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '${model.item.brandName}',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ),
-        ),
-        // 가격 정보를 위한 SliverToBoxAdapter
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  "${model.item.discountPrice}원",
-                  style: TextStyle(fontSize: 18, color: Colors.red),
-                ),
-                SizedBox(width: 8),
-                Text(
-                  "${model.item.price}원",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      decoration: TextDecoration.lineThrough),
-                ),
-              ],
-            ),
-          ),
-        ),
-        SliverToBoxAdapter(
-          child: SizedBox(height: 60),
-        ),
-        SliverToBoxAdapter(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height, // 미디어 쿼리 대신 고정 높이 사용
-            child: ListView.builder(
-              itemCount: model.detailPhotos.length,
-              itemBuilder: (context, index) {
-                return Image.memory(
-                  Base64Decoder()
-                      .convert(model.detailPhotos[index].subPhotoBase64),
-                  fit: BoxFit.fill,
-                );
-              },
-            ),
-          ),
-        ),
-        // 추가 정보를 위한 SliverList
         // 다른 Sliver 위젯들을 더 추가할 수 있습니다.
       ],
     );
