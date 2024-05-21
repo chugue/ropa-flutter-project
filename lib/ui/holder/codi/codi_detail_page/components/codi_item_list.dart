5b63543d8a22ac3549a1e2c1ea2008510c16abe7
LifeIsOne <didtmdgh258@gmail.com>
2024-05-21 23:12:39 +0900

    특이점 수정

--- lib/ui/holder/codi/codi_detail_page/components/codi_item_list.dart
+++ lib/ui/holder/codi/codi_detail_page/components/codi_item_list.dart
@@ -3,6 +3,7 @@ import 'package:final_project_team02/ui/holder/codi/codi_detail_page/codi_detail
 import 'package:final_project_team02/ui/holder/item/item_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
+
 import '../../../../../_core/constants/http.dart';
 
 class ItemList extends StatelessWidget {
@@ -16,58 +17,53 @@ class ItemList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 200,
-      child: PageView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, CodiIndex) {
-          return SizedBox(
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: model!.itemPhotos.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        print("사진 클릭 됨");
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ItemPage(
-                                  itemId: model!.itemPhotos[index].itemsId)),
-                        );
-                      },
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.40,
-                        height: MediaQuery.of(context).size.width * 0.40,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 5.0),
-                          child: Container(
-                            width: double.infinity, // 셀의 전체 너비 사용
-                            height: double.infinity, // 셀의 전체 높이 사용
-                            child: CachedNetworkImage(
-                              imageUrl:
-                                  '$baseURL${model!.itemPhotos[index].photoPath}',
-                              placeholder: (context, url) => Shimmer.fromColors(
-                                baseColor: Colors.grey[300]!,
-                                highlightColor: Colors.grey[100]!,
-                                child: Container(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  Icon(Icons.error),
-                              fit: BoxFit.cover,
+      child: SizedBox(
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: model!.itemPhotos.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print("사진 클릭 됨");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ItemPage(
+                              itemId: model!.itemPhotos[index].itemsId)),
+                    );
+                  },
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.40,
+                    height: MediaQuery.of(context).size.width * 0.40,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 5.0),
+                      child: Container(
+                        width: double.infinity, // 셀의 전체 너비 사용
+                        height: double.infinity, // 셀의 전체 높이 사용
+                        child: CachedNetworkImage(
+                          imageUrl:
+                              '$baseURL${model!.itemPhotos[index].photoPath}',
+                          placeholder: (context, url) => Shimmer.fromColors(
+                            baseColor: Colors.grey[300]!,
+                            highlightColor: Colors.grey[100]!,
+                            child: Container(
+                              color: Colors.white,
                             ),
                           ),
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
-                  ],
-                );
-              },
-            ),
-          );
-        },
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
